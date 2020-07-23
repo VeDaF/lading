22ffb2ffeec93b1f0c13643e614bbbb1016dd25f
islam <vedafv@gmail.com>
2020-07-23 14:23:42 +0600

    new es6

--- js/script.js
+++ js/script.js
@@ -1,98 +1,114 @@
 window.addEventListener('DOMContentLoaded', function () {
-    'use strict';
-    let tab = document.querySelectorAll('.info-header-tab'),
-        info = document.querySelector('.info-header'),
-        tabContent = document.querySelectorAll('.info-tabcontent');
+  'use strict';
+  let tab = document.querySelectorAll('.info-header-tab'),
+    info = document.querySelector('.info-header'),
+    tabContent = document.querySelectorAll('.info-tabcontent');
 
-    function hideTabContent(a) {
-        for (let i = a; i < tabContent.length; i++) {
-            tabContent[i].classList.remove('show');
-            tabContent[i].classList.add('hide');
-        }
+  function hideTabContent(a) {
+    for (let i = a; i < tabContent.length; i++) {
+      tabContent[i].classList.remove('show');
+      tabContent[i].classList.add('hide');
     }
+  }
 
-    hideTabContent(1);
+  hideTabContent(1);
 
-    function showTabContent(b) {
-        if (tabContent[b].classList.contains('hide')) {
-            tabContent[b].classList.remove('hide');
-            tabContent[b].classList.add('show');
-        }
+  function showTabContent(b) {
+    if (tabContent[b].classList.contains('hide')) {
+      tabContent[b].classList.remove('hide');
+      tabContent[b].classList.add('show');
     }
+  }
 
-    info.addEventListener('click', function (event) {
-        let target = event.target;
-        if (target && target.classList.contains('info-header-tab')) {
-            for (let i = 0; i < tab.length; i++) {
-                if (target == tab[i]) {
-                    hideTabContent(0);
-                    showTabContent(i);
-                    break;
-                }
-            }
+  info.addEventListener('click', function (event) {
+    let target = event.target;
+    if (target && target.classList.contains('info-header-tab')) {
+      for (let i = 0; i < tab.length; i++) {
+        if (target == tab[i]) {
+          hideTabContent(0);
+          showTabContent(i);
+          break;
         }
-    });
-
-    // Timer
-
-    let deadline = '2020-08-21';
-
-    function getTimeRemaining(endtime) {
-        let t = Date.parse(endtime) - Date.parse(new Date()),
-            seconds = Math.floor((t / 1000) % 60),
-            minutes = Math.floor((t / 1000 / 60) % 60),
-            hours = Math.floor(t / (1000 * 60 * 60));
-
-        return {
-            total: t,
-            hours: hours,
-            minutes: minutes,
-            seconds: seconds,
-        };
+      }
     }
+  });
 
-    function setClock(id, endtime) {
-        let timer = document.getElementById(id),
-            hours = timer.querySelector('.hours'),
-            minutes = timer.querySelector('.minutes'),
-            seconds = timer.querySelector('.seconds'),
-            timeInterval = setInterval(updateClock, 1000);
+  // Timer
 
-        function updateClock() {
-            let t = getTimeRemaining(endtime);
+  let deadline = '2020-08-21';
 
-            function addZero(num) {
-                if (num <= 9) {
-                    return '0' + num;
-                } else return num;
-            }
+  function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+      seconds = Math.floor((t / 1000) % 60),
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      hours = Math.floor(t / (1000 * 60 * 60));
 
-            hours.textContent = addZero(t.hours);
-            minutes.textContent = addZero(t.minutes);
-            seconds.textContent = addZero(t.seconds);
+    return {
+      total: t,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
 
-            if (t.total <= 0) {
-                clearInterval(timeInterval);
-                hours.textContent = '00';
-                minutes.textContent = '00';
-                seconds.textContent = '00';
-            }
-        }
+  function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+      hours = timer.querySelector('.hours'),
+      minutes = timer.querySelector('.minutes'),
+      seconds = timer.querySelector('.seconds'),
+      timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      let t = getTimeRemaining(endtime);
+
+      function addZero(num) {
+        if (num <= 9) {
+          return '0' + num;
+        } else return num;
+      }
+
+      hours.textContent = addZero(t.hours);
+      minutes.textContent = addZero(t.minutes);
+      seconds.textContent = addZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+        hours.textContent = '00';
+        minutes.textContent = '00';
+        seconds.textContent = '00';
+      }
     }
+  }
 
-    setClock('timer', deadline);
-    let more = document.querySelector('.more'),
-        overlay = document.querySelector('.overlay'),
-        close = document.querySelector('.popup-close');
-    more.addEventListener('click', function () {
-        overlay.style.display = 'block';
-        this.classList.add('more-splash');
-        document.body.style.overflow = 'hidden';
-    });
-    close.addEventListener('click', function () {
-        overlay.style.display = 'none';
-        more.classList.remove('more-splash');
-        document.body.style.overflow = '';
-    });
-
+  setClock('timer', deadline);
+  let more = document.querySelector('.more'),
+    overlay = document.querySelector('.overlay'),
+    close = document.querySelector('.popup-close');
+  more.addEventListener('click', function () {
+    overlay.style.display = 'block';
+    this.classList.add('more-splash');
+    document.body.style.overflow = 'hidden';
+  });
+  close.addEventListener('click', function () {
+    overlay.style.display = 'none';
+    more.classList.remove('more-splash');
+    document.body.style.overflow = '';
+  });
 });
+class Options {
+  constructor(height, width, bg, fontSize, textAlign) {
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.textAlign = textAlign;
+  }
+  createDiv() {
+    let elem = document.createElement('div');
+    document.body.appendChild(elem);
+    let param = `height: ${this.height}px width: ${this.width}px bg: ${this.bg} fontSize: ${this.fontSize}px textAlign: ${this.textAlign}`;
+    elem.style.cssText = param;
+  }
+}
+const item = new Options(300, 350, 'red', 14, 'center');
+item.createDiv();
